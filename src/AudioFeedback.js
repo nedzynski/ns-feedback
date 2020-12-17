7aa75bf89af600828b2847e9fcda2795299f4a22
nedzynski <nedzynski@gmail.com>
2020-12-17 16:59:09 +0100

    Queuing feedbacks messages

--- src/AudioFeedback.js
+++ src/AudioFeedback.js
@@ -1,5 +1,11 @@
+const successSound =
+  "https://freesound.org/data/previews/173/173000_2193194-lq.mp3";
+const failureSound =
+  "https://freesound.org/data/previews/342/342756_5260872-lq.mp3";
+
 class AudioFeedback {
   constructor() {
+    this.playQueue = [];
     this.failureFeedbacks = [
       "https://freesound.org/data/previews/182/182271_84709-lq.mp3",
       "https://freesound.org/data/previews/33/33276_286533-lq.mp3",
@@ -10,9 +16,24 @@ class AudioFeedback {
       "https://freesound.org/data/previews/266/266916_4657534-lq.mp3"
     ];
   }
-  selectFeedback () {
-    return this.failureFeedbacks[Math.floor(Math.random() * this.failureFeedbacks.length)]
+  wipeFeedback() {
+    this.playQueue = [];
   }
-};
+  queueFeedback(isSuccess) {
+    if (isSuccess) {
+      this.playQueue.push(successSound);
+    } else {
+      this.playQueue.push(failureSound);
+      this.playQueue.push(
+        this.failureFeedbacks[
+          Math.floor(Math.random() * this.failureFeedbacks.length)
+        ]
+      );
+    }
+  }
+  selectFeedback() {
+    return this.playQueue.shift();
+  }
+}
 
 export default AudioFeedback;
--- src/Verify.js
+++ src/Verify.js
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { isValidElement } from "react";
 import Sound from "react-sound";
 
 import AudioFeedback from "./AudioFeedback";
-
-const successSound = "https://freesound.org/data/previews/173/173000_2193194-lq.mp3";
-const failureSound = "https://freesound.org/data/previews/342/342756_5260872-lq.mp3"
 
 class Verify extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       audioFeedbackSelector: new AudioFeedback(),
-      url: failureSound,
+      url: "",
       playStatus: Sound.status.STOPPED
     };
   }
@@ -20,12 +17,24 @@ class Verify extends React.Component {
     soundManager.setup({ debugMode: false });
   }
 
+  play() {
+    var feedbackPiece = this.state.audioFeedbackSelector.selectFeedback();
+    if (feedbackPiece) {
+      this.setState({ 
+        url: feedbackPiece,
+        playStatus: Sound.status.PLAYING 
+      });  
+    } else {
+      this.setState({ playStatus: Sound.status.STOPPED })
+    }
+  }
+  
   verify() {
     console.log(this.props.result);
-    this.setState({ 
-      url: this.props.result === "success" ? successSound : this.state.audioFeedbackSelector.selectFeedback(),
-      playStatus: Sound.status.PLAYING 
-    });
+    var isSuccess = this.props.result === "success";
+    this.state.audioFeedbackSelector.wipeFeedback();
+    this.state.audioFeedbackSelector.queueFeedback(isSuccess);
+    this.play();
   }
   render() {
     return (
@@ -35,7 +44,7 @@ class Verify extends React.Component {
           url={this.state.url}
           playStatus={this.state.playStatus}
           autoLoad={true}
-          onFinishedPlaying={() => this.setState({ playStatus: Sound.status.STOPPED })}
+          onFinishedPlaying={() => this.play()}
         />
       </button>
     );
